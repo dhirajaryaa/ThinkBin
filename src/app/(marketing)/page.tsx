908d9201f3dc4721384a29361bcfd8e5a25fc9ba
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-11-10 10:19:34 +0000

    feat: feature added on landing page

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,9 +1,11 @@
+import Feature from "@/components/landing/Feature";
 import Hero from "@/components/landing/Hero";
 
 export default function Page() {
   return (
     <main className="px-8 sm:px-12">
       <Hero />
+      <Feature />
     </main>
   );
 }
