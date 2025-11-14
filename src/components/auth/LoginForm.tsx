968631219507600601317399588ed392f0b2e2e8
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-11-14 01:04:42 +0000

    feat: react-form-hook add on login page

--- src/components/auth/LoginForm.tsx
+++ src/components/auth/LoginForm.tsx
@@ -1,43 +1,60 @@
-import { cn } from "@/lib/utils"
-import { Button } from "@/components/ui/button"
+"use client";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
+} from "@/components/ui/card";
 import {
   Field,
   FieldDescription,
   FieldGroup,
   FieldLabel,
   FieldSeparator,
-} from "@/components/ui/field"
-import { Input } from "@/components/ui/input"
-import Google from "@/components/icons/Google"
-import Link from "next/link"
-import Password from "./Password"
+} from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import Google from "@/components/icons/Google";
+import Link from "next/link";
+import Password from "./Password";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type Inputs = {
+  email: string;
+  password: string;
+};
 
 export function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
+
   return (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card>
         <CardHeader className="text-center">
           <CardTitle className="text-xl">Welcome back</CardTitle>
-          <CardDescription>
-            Login with your Google account
-          </CardDescription>
+          <CardDescription>Login with your Google account</CardDescription>
         </CardHeader>
         <CardContent>
-          <form>
+          <form onSubmit={handleSubmit(onSubmit)}>
             <FieldGroup>
               <Field>
-                <Button variant="outline" className="bg-muted cursor-pointer" type="button">
-                 <Google />
+                <Button
+                  variant="outline"
+                  className="bg-muted cursor-pointer"
+                  type="button"
+                >
+                  <Google />
                   Login with Google
                 </Button>
               </Field>
@@ -47,11 +64,11 @@ export function LoginForm({
               <Field>
                 <FieldLabel htmlFor="email">Email</FieldLabel>
                 <Input
-                  id="email"
-                  type="email"
+                  {...register("email", { required: "Email is required" })}
+                  aria-invalid={errors.email ? "true" : "false"}
                   placeholder="m@example.com"
-                  required
                 />
+                {errors.email && <p className="text-destructive text-xs">{errors.email.message}</p>}
               </Field>
               <Field>
                 <div className="flex items-center">
@@ -63,12 +80,13 @@ export function LoginForm({
                     Forgot your password?
                   </a> */}
                 </div>
-                <Password />
+                <Password register={register} errors={errors} />
               </Field>
               <Field>
                 <Button type="submit">Login</Button>
                 <FieldDescription className="text-center">
-                  Don&apos;t have an account? <Link href="/signup">Sign up</Link>
+                  Don&apos;t have an account?{" "}
+                  <Link href="/signup">Sign up</Link>
                 </FieldDescription>
               </Field>
             </FieldGroup>
@@ -76,5 +94,5 @@ export function LoginForm({
         </CardContent>
       </Card>
     </div>
-  )
+  );
 }
--- src/components/auth/Password.tsx
+++ src/components/auth/Password.tsx
@@ -3,18 +3,28 @@ import { Input } from "@/components/ui/input";
 import { Eye, EyeClosed } from "lucide-react";
 import { useState } from "react";
 
-function Password() {
+function Password({ register, errors }: any) {
   const [showPassword, setShowPassword] = useState(false);
   return (
-    <div className="flex relative w-full items-center">
-      <Input id="password" type={showPassword ? "text" : "password"} placeholder="xyz@123" required />
+    <div className="relative space-y-3 w-full items-center">
+      <Input
+        id="password"
+        type={showPassword ? "text" : "password"}
+        {...register("password", { required: "Password is required" })}
+        aria-invalid={errors.password ? "true" : "false"}
+        placeholder="xyz@123"
+      />
+
       <button
         onClick={() => setShowPassword(!showPassword)}
-        className="absolute right-2 cursor-pointer"
+        className="absolute right-2 top-3 cursor-pointer"
         type="button"
       >
         {showPassword ? <EyeClosed size={18} /> : <Eye size={18} />}
       </button>
+      {errors.password && (
+        <p className="text-destructive text-xs">{errors.password.message}</p>
+      )}
     </div>
   );
 }
