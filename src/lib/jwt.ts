f0d1657508d9427758e12e2502e6e013a8b922a5
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-11-15 01:05:22 +0000

    feat: make sign and verify jwt utils [using jose package]

---
+++ src/lib/jwt.ts
@@ -0,0 +1,41 @@
+import { JWTPayload, SignJWT, jwtVerify } from 'jose';
+
+// types
+export interface TokenPayload extends JWTPayload {
+    id: string;
+    email: string;
+    name: string;
+}
+
+const accessSecret = new TextEncoder().encode(process.env.R_TOKEN_SECRET!);
+const refreshSecret = new TextEncoder().encode(process.env.REFRESH_TOKEN_SECRET!);
+
+
+//  sign token 
+export async function signAccessAndRefreshToken(payload: TokenPayload) {
+    const accessToken = await new SignJWT(payload)
+        .setExpirationTime(process.env.ACCESS_TOKEN_EXPIRY!)
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .sign(accessSecret);
+    const refreshToken = await new SignJWT({ id: payload.id })
+        .setExpirationTime(process.env.REFRESH_TOKEN_EXPIRY!)
+        .setProtectedHeader({ alg: "HS256" })
+        .setIssuedAt()
+        .sign(refreshSecret);
+
+    return {
+        accessToken,
+        refreshToken
+    }
+}
+
+// verify token 
+export async function verifyToken(token: string, SECRET: Uint8Array = accessSecret) {
+    try {
+        const { payload } = await jwtVerify(token, SECRET);
+        return payload;
+    } catch (err) {
+        return null;
+    }
+}
