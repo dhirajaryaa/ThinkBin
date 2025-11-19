3b507062fa56c03dc96ba199095c6fc72dbd2a1a
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-11-19 01:42:19 +0000

    refactor: add auth user redirect on dashboard

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,7 +1,11 @@
+import { getCurrentUser } from "@/actions/auth/getCurrentUser";
 import { SignUpForm } from "@/components/auth/SignupForm";
 import Logo from "@/components/common/Logo";
+import { redirect } from "next/navigation";
 
-export default function SignupPage() {
+export default async function SignupPage() {
+    const user = await getCurrentUser();
+    if (user) redirect("/dashboard");
   return (
     <main className="bg-muted flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
       <section className="flex w-full max-w-sm flex-col gap-6">
