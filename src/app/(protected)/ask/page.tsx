73c629181a0b8f0a6fdc88dc9beae016b9014142
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-11-23 16:16:40 +0000

    feat: create Ask Memories page and refactor Dashboard page layout

--- src/app/(protected)/ask/page.tsx
+++ src/app/(protected)/ask/page.tsx
@@ -4,11 +4,11 @@ export default function DashboardPage() {
   return (
     <>
     <AppHeader>
-      <h1 className="text-xl font-bold">Dashboard</h1>
+      <h1 className="text-xl font-bold">Ask Memories</h1>
     </AppHeader>
     <main className="w-full relative">
       <div className="w-full tracking-wide py-8 text-4xl xs:text-5xl sm:text-7xl lg:text-9xl font-black text-center text-foreground/20 select-none uppercase font-serif my-30">
-        DashBoard
+        Chat with Memories
       </div>
     </main>
     </>
